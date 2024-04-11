e3bb69b17c6c8c8a3760f6d3d12b75aae0bf05d1
David Hernandez <dhern167@calpoly.edu>
2024-04-11 15:26:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+// import {Map, View} from 'ol';
+// import TileLayer from 'ol/layer/Tile';
+// import OSM from 'ol/source/OSM';
+
+
+import Map from 'https://cdn.jsdelivr.net/npm/ol/Map.js';
+import View from 'https://cdn.jsdelivr.net/npm/ol/View.js';
+import TileLayer from 'https://cdn.jsdelivr.net/npm/ol/layer/Tile.js';
+import OSM from 'https://cdn.jsdelivr.net/npm/ol/source/OSM.js';
+
 
 const map = new Map({
   target: 'map',
