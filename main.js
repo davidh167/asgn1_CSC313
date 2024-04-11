0567d6e0406129ccab4b15817ade02bd2397e576
David Hernandez <dhern167@calpoly.edu>
2024-04-11 16:45:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css';
-import {Map, View} from 'ol';
+// import {Map, View} from 'ol';
+import Map from 'ol/Map';
+import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
